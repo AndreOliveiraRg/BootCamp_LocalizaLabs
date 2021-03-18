f3d5fcc64a4facef59095ffe95b2a811ece81987
André Luiz Terrozo de Oliveira <andre.oliveirarg@outlook.com>
2021-03-17 22:12:54 -0300

    add exercicios conta bancária.

---
+++ 2_AplicacaoTranferenciaBancaria/Classes/Conta.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2_AplicacaoTranferenciaBancaria
+{
+    public class Conta
+    {
+        private TipoConta TipoConta { get; set; }
+        private double Saldo { get; set; }
+        private double Credito { get; set; }
+        private string Nome { get; set; }
+
+        public Conta(TipoConta tipoConta, double saldo, double credito, string nome)
+        {
+            this.TipoConta = tipoConta;
+            this.Saldo = saldo;
+            this.Credito = credito;
+            this.Nome = nome;
+        }
+
+        public bool Sacar(double valorSaque)
+        {
+            if (this.Saldo - valorSaque < (this.Credito * .1))
+            {
+                Console.WriteLine("Saldo insuficiente");
+                return false;
+            }
+
+            this.Saldo -= valorSaque;
+
+            Console.WriteLine($"Saldo atual da conta de {this.Nome} é {this.Saldo}");
+            return true;
+        }
+
+        public void Depositar(double valorDeposito)
+        {
+            this.Saldo += valorDeposito;
+            Console.WriteLine($"Saldo atual da conta de {this.Nome} é {this.Saldo}");
+        }
+
+        public void Transferir(double valorTranferencia, Conta contaDestino)
+        {
+            if (this.Sacar(valorTranferencia))
+                contaDestino.Depositar(valorTranferencia);
+
+            Console.WriteLine($"Saldo atual da conta de {this.Nome} é {this.Saldo}");
+        }
+
+        public string ObterNome()
+        {
+            return this.Nome;
+        }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += $"TipoConta {this.TipoConta} | ";
+            retorno += $"Nome {this.Nome} | ";
+            retorno += $"Saldo {this.Saldo} | ";
+            retorno += $"Crédito {this.Credito}";
+            return retorno;
+        }
+    }
+}
---
+++ 2_AplicacaoTranferenciaBancaria/Program.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2_AplicacaoTranferenciaBancaria
+{
+    class Program
+    {
+        static List<Conta> listContas = new List<Conta>();
+
+        static void Main(string[] args)
+        {
+            Conta minhaConta = new Conta(TipoConta.PessoaFisica, 0, 0, "André Oliveira");
+
+            string opcaoUsuario = ObterOpcaoUsuario();
+
+            while (opcaoUsuario != "X")
+            {
+                switch (opcaoUsuario)
+                {
+                    case "1":
+                        ListarContas();
+                        break;
+                    case "2":
+                        InserirContas();
+                        break;
+                    case "3":
+                    Transferir();
+                        break;
+                    case "4":
+                        Sacar();
+                        break;
+                    case "5":
+                    Depositar();
+                        break;
+                    case "C":
+                        Console.Clear();
+                        opcaoUsuario = ObterOpcaoUsuario();
+                        break;
+                    default:
+                        break;
+                }
+
+                opcaoUsuario = ObterOpcaoUsuario();
+            }
+
+            Console.WriteLine("Obrigado por utilizar nossos serviços.");
+            Console.ReadLine();
+        }
+        private static void Sacar()
+        {
+            Console.WriteLine("Digite o número da conta: ");
+            int indiceConta = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor a ser sacado: ");
+            double valorDeposito = double.Parse(Console.ReadLine());
+
+            listContas[indiceConta].Sacar(valorDeposito);
+        }
+        private static void Transferir()
+        {
+            Console.WriteLine("Digite o número da conta de origem: ");
+            int indiceConta = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o número da conta de destino: ");
+            int indiceContaDestino = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor a ser transferido: ");
+            double valorDeposito = double.Parse(Console.ReadLine());
+
+            listContas[indiceConta].Transferir(valorDeposito, listContas[indiceContaDestino]);
+        }
+        private static void Depositar()
+        {
+            Console.WriteLine("Digite o número da conta: ");
+            int indiceConta = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor a ser depositado: ");
+            double valorDeposito = double.Parse(Console.ReadLine());
+                        
+            listContas[indiceConta].Depositar(valorDeposito);
+        }
+        private static void InserirContas()
+        {
+            Console.WriteLine("Inserir nova conta");
+
+            Console.WriteLine("Digite 1 para Conta Física ou 2 para Jurídica: ");
+            int tipoConta = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Digite o Nome do Cliente: ");
+            string nome = Console.ReadLine();
+
+            Console.WriteLine("Digite o saldo inicial: ");
+            double saldo = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Digite o crédito: ");
+            double credito = Convert.ToDouble(Console.ReadLine());
+
+            if (!listContas.Any(x => x.ObterNome().Equals(nome)))
+                listContas.Add(new Conta(tipoConta: (TipoConta)tipoConta, saldo: saldo, credito: credito, nome: nome));
+        }
+        private static void ListarContas()
+        {
+            Console.WriteLine("Listar contas");
+
+            if (!listContas.Any())
+            {
+                Console.WriteLine("Nenhuma conta cadastrada.");
+                return;
+            }
+
+            for (int i = 0; i < listContas.Count; i++)
+            {
+                Conta conta = listContas[i];
+
+                Console.Write("#{0} --- ", i);
+                Console.WriteLine(conta.ToString());
+            }
+        }
+        private static string ObterOpcaoUsuario()
+        {
+            Console.WriteLine();
+            Console.WriteLine("DIO Bank a seu dispor!!");
+            Console.WriteLine("Informe a opção desejada:");
+
+            Console.WriteLine("1- Listar contas");
+            Console.WriteLine("2- Inserir nova conta");
+            Console.WriteLine("3- Transferir");
+            Console.WriteLine("4- Sacar");
+            Console.WriteLine("5- Depositar");
+            Console.WriteLine("C- Limpar Tela");
+            Console.WriteLine("X- Sair");
+
+            string opcaoUsuario = Console.ReadLine().ToUpper();
+            Console.WriteLine();
+            return opcaoUsuario;
+
+        }
+    }
+}
